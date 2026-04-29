90b5090e6243a13966a08d2a75a32e0da7bd284f
Shreya C <97959780+ShreyAC962@users.noreply.github.com>
2026-04-28 22:48:28 -0700

    Established database connection

--- app/models.py
+++
@@ -1,9 +0,0 @@
-# To establish connection to database and communicates with PostgreSQL
-from sqlalchemy import create_engine
-# To cretae database sessions - Is used to interact with the database(run queries, insert data, etc)
-from sqlalchemy.orm import sessionmaker
-from app.config import DB_URL
-
-engine = create_engine(DB_URL)
-
-SessionLocal = sessionmaker(bind=engine)
